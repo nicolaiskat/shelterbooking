20075bb62c64e4958e61c8bab104238bd90cb6f8
nicolaiskat <jegerrockstar@gmail.com>
2021-10-26 11:02:29 +0200

    dsa

---
+++ shelterbooking/shelterbooking/Server/SheltersDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace shelterbooking.Server
+{
+    public class SheltersDatabaseSettings : ISheltersDatabaseSettings
+    {
+        public SheltersDatabaseSettings()
+        {
+        }
+    }
+    public class ISheltersDatabaseSettings
+    {
+        public ISheltersDatabaseSettings()
+        {
+        }
+    }
+}
