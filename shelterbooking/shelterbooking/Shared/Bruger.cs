3e35646c06fb5b861731bf2f37df38c18de0a8c9
Alexander Jensen <alexanderjensen@MacBook-Air-tilhrende-Alexander.local>
2021-10-26 10:34:24 +0200

    bruger class bruger class

---
+++ shelterbooking/shelterbooking/Shared/Bruger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace shelterbooking.Shared
+{
+    public class Bruger
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        public string fornavn { get; set; }
+        public string efternavn { get; set; }
+        public int mobil { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public Access access { get; set; }
+    }
+    public class Access
+    {
+        public int level { get; set; }
+        public string beskr { get; set; }
+    }
+}
