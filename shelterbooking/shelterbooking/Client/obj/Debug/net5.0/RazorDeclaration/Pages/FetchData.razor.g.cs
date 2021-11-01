2ba7f0c4a49c6a8743caa920213ae78b608e222c
SisselSchmidt <82371023+SisselSchmidt@users.noreply.github.com>
2021-11-01 10:51:37 +0100

    login

--- shelterbooking/shelterbooking/Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ shelterbooking/shelterbooking/Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -98,7 +98,7 @@ using shelterbooking.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 38 "C:\Users\sisse\Documents\GitHub\shelterbooking\shelterbooking\shelterbooking\Client\Pages\FetchData.razor"
+#line 59 "C:\Users\sisse\Documents\GitHub\shelterbooking\shelterbooking\shelterbooking\Client\Pages\FetchData.razor"
        
     private Shelter[] shelters;
 
--- shelterbooking/shelterbooking/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ shelterbooking/shelterbooking/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -90,7 +90,7 @@ using shelterbooking.Client.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\sisse\Documents\GitHub\shelterbooking\shelterbooking\shelterbooking\Client\Shared\NavMenu.razor"
+#line 33 "C:\Users\sisse\Documents\GitHub\shelterbooking\shelterbooking\shelterbooking\Client\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
